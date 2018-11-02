9364d2cb2a2ae0995619825df648ec62ea01a969
Emily Fortuna <efortuna@google.com>
2018-11-02 13:28:42 -0700

    added camera plugin

--- lib/photo_clock.dart
+++ lib/photo_clock.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:final_countdown/countdown_stream.dart';
 import 'package:final_countdown/utils.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:async';
+import 'dart:io';
 
 class PhotoClock extends StatelessWidget {
   PhotoClock(this.countdown);
@@ -36,32 +39,63 @@ class Picture extends StatefulWidget {
 
 class _PictureState extends State<Picture> {
   Widget _image;
-  // Normalize rating to (0,1) and interpolate color from green to red as we run out of time
   Color _color;
   StreamSubscription _colorUpdates;
+  CameraController _controller;
 
   @override
   initState() {
     super.initState();
+    initializeCamera();
     _image = timer();
-    _color = Colors.green;
+    _color = Colors.yellow;
     // TODO(efortuna): I feel like there should be a better way to do this.
     _colorUpdates = widget.countdown.time.listen((Duration newDuration) {
-      setState(() {_color = Color.lerp(
-                  Colors.red,
-                  Colors.green,
-                  newDuration.inMinutes /
-                      widget.countdown.duration.inMinutes);
-              });
+      // Normalize rating to (0,1) and interpolate color from yellow to red as we run out of time
+      setState(() {
+        _color = Color.lerp(Colors.red, Colors.yellow,
+            newDuration.inMinutes / widget.countdown.duration.inMinutes);
+      });
     });
+  }
+
+  initializeCamera() async {
+    List<CameraDescription> cameraOptions = await availableCameras();
+    try {
+      var frontCamera = cameraOptions.firstWhere((description) =>
+          description.lensDirection == CameraLensDirection.front);
+      _controller = CameraController(frontCamera, ResolutionPreset.low);
+      await _controller.initialize();
+    } on StateError catch (e) {
+      print('No front-facing camera found: $e');
+    }
+  }
+
+  Future<String> takePicture() async {
+    assert(_controller.value.isInitialized);
+    Directory extDir = await getApplicationDocumentsDirectory();
+    var dirPath = '${extDir.path}/Pictures/clock_app';
+    await Directory(dirPath).create(recursive: true);
+    var filePath = '$dirPath/${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+    try {
+      await _controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      print('There was a problem taking the picture. $e');
+      return null;
+    }
+    return filePath;
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
-      child:
-          AnimatedContainer(height: 100.0, width: 100.0, color: _color, child: _image, duration: const Duration(milliseconds: 500)),
+      child: AnimatedContainer(
+          height: 100.0,
+          color: _color,
+          child: _image,
+          duration: const Duration(milliseconds: 500)),
     );
   }
 
@@ -70,7 +104,7 @@ class _PictureState extends State<Picture> {
     _colorUpdates.cancel();
     super.deactivate();
   }
-  
+
   timer() => StreamBuilder(
         stream: widget.countdown.time,
         builder: (context, AsyncSnapshot<Duration> snapshot) {
