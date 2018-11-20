30f5a63da19c8038151b6dfba3a713b4ff7ccfce
Matt Sullivan <matt.j.sullivan@gmail.com>
2018-11-20 14:43:57 -0800

    Changes to RetroClock styling

--- lib/clocks/retro_clock.dart
+++ lib/clocks/retro_clock.dart
@@ -1,8 +1,6 @@
 // Inspired by: https://medium.com/flutter-community/make-3d-flip-animation-in-flutter-16c006bb3798
 import 'package:flutter/material.dart';
-
 import 'package:flip_panel/flip_panel.dart';
-
 import 'package:final_countdown/data/countdown_provider.dart';
 
 class RetroClock extends StatelessWidget {
@@ -12,18 +10,18 @@ class RetroClock extends StatelessWidget {
       alignment: AlignmentDirectional.center,
       children: <Widget>[
         Positioned.fill(
-          child: Image.asset('assets/wood.jpg', fit: BoxFit.cover),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 200.0, horizontal: 10.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.white, width: 10.0),
-                borderRadius: BorderRadius.circular(20)),
-            color: Colors.grey[800],
-            elevation: 4,
-            child: ClockPanel(),
+          child: Image.asset(
+            'assets/wood.jpg',
+            fit: BoxFit.cover,
           ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.white, width: 10.0),
+              borderRadius: BorderRadius.circular(20)),
+          color: Colors.grey[800],
+          elevation: 4,
+          child: FittedBox(child: ClockPanel()),
         ),
       ],
     );
@@ -42,10 +40,13 @@ class ClockPanel extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             StreamFlipDigit(countdown.tensMinuteDigitStream),
+            SizedBox(width: 5),
             StreamFlipDigit(countdown.onesMinuteDigitStream),
-            //SizedBox(width: 50),
+            SizedBox(width: 5),
             FlipBox(':'),
+            SizedBox(width: 5),
             StreamFlipDigit(countdown.tensSecondDigitStream),
+            SizedBox(width: 5),
             StreamFlipDigit(countdown.onesSecondDigitStream),
           ]),
     );
@@ -61,10 +62,9 @@ class StreamFlipDigit extends StatelessWidget {
     return Container(
       child: FlipPanel<int>.stream(
           itemStream: digitStream,
-          initValue: 7, // TODO: Matt -- Is this intentional?
           direction: FlipDirection.down,
           itemBuilder: (context, digit) {
-            return FlipBox(digit.toString());
+            return FlipBox('$digit');
           }),
     );
   }
@@ -83,99 +83,18 @@ class FlipBox extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
+        child: Container(
           width: 40,
-          height: 50,
+          height: 60,
           child: FittedBox(
             fit: BoxFit.contain,
-            child: Text(
-              str,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 60,
-                  // TODO: Consider setting font too. This one could be cool: 
-                  // https://fonts.google.com/specimen/Wallpoet
-                  color: Colors.white),
-            ),
+            child: Text(str,
+                style: TextStyle(
+                    // fontFamily: 'Orbitron',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                    color: Colors.white)),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class StreamFlipClock extends StatelessWidget {
-  final _spacing = const EdgeInsets.symmetric(horizontal: 2);
-  static final _digitSize = 90.0;
-  static final _panelHeight = _digitSize + 20;
-  static final _textStyle = TextStyle(
-      fontWeight: FontWeight.bold, fontSize: _digitSize, color: Colors.white);
-  final _borderRadius = const BorderRadius.all(Radius.circular(3));
-
-  Widget _buildDigit(Stream<int> streamSource, int startValue) {
-    return Padding(
-      padding: _spacing,
-      child: FlipPanel<int>.stream(
-        itemStream: streamSource,
-        // TODO(efortuna): Get "startValue" from "preferences" somehow don't always show 15
-        initValue: startValue,
-        direction: FlipDirection.down,
-        itemBuilder: (context, digit) => Container(
-              alignment: Alignment.center,
-              width: _digitSize - 20,
-              height: _panelHeight,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: _borderRadius,
-              ),
-              child: Text(
-                '$digit',
-                style: _textStyle,
-              ),
-            ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final countdown = CountdownProvider.of(context);
-    return Center(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          // Minutes
-          _buildDigit(
-              countdown.tensMinuteDigitStream, countdown.tensMinuteDigit),
-          _buildDigit(
-              countdown.onesMinuteDigitStream, countdown.onesMinuteDigit),
-
-          _buildSeparator(),
-
-          // Seconds
-          _buildDigit(
-              countdown.tensSecondDigitStream, countdown.tensSecondDigit),
-          _buildDigit(
-              countdown.onesSecondDigitStream, countdown.onesSecondDigit),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSeparator() {
-    return Padding(
-      padding: _spacing,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: _borderRadius,
-        ),
-        width: _digitSize / 4,
-        height: _panelHeight,
-        alignment: Alignment.center,
-        child: Text(
-          ':',
-          style: _textStyle,
         ),
       ),
     );
