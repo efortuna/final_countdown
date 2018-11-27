3568783c8a20d434182b1784301c7aae7fa57326
Emily Fortuna <efortuna@google.com>
2018-11-26 17:18:37 -0800

    more simplifications

--- lib/clocks/photo_clock.dart
+++ lib/clocks/photo_clock.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-
-import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'package:final_countdown/data/countdown_provider.dart';
 import 'package:final_countdown/data/file_stream_provider.dart';
 import 'package:final_countdown/utils.dart';
+import 'package:final_countdown/styling.dart';
 import 'package:final_countdown/clocks/simple_clock.dart';
 
 class PhotoClock extends StatelessWidget {
@@ -28,45 +25,17 @@ class Photographer extends StatefulWidget {
 }
 
 class _PhotographerState extends State<Photographer> {
-  CameraController _camera;
-  CameraLensDirection _cameraDirection;
   StreamSubscription _countdownSubscription;
-
+  bool _frontCamera;
+  Camera _camera;
   @override
   void initState() {
-    _cameraDirection = CameraLensDirection.front;
-    takePicture();
+    _frontCamera = true;
+    _camera = Camera()..takePicture();
     _countdownSubscription = widget.countdown.stream.listen((Duration d) {
-      if (d.inSeconds % 60 == 0) takePicture();
+      if (d.inSeconds % 60 == 0) _camera.takePicture();
     });
     super.initState();
-  }
-
-  initializeCamera() async {
-    List<CameraDescription> cameraOptions = await availableCameras();
-    try {
-      var frontCamera = cameraOptions.firstWhere(
-          (description) => description.lensDirection == _cameraDirection,
-          orElse: () => cameraOptions.first);
-      _camera = CameraController(frontCamera, ResolutionPreset.low);
-      await _camera.initialize();
-    } on StateError catch (e) {
-      print('No camera found in the direction $_cameraDirection: $e');
-    }
-  }
-
-  takePicture() async {
-    if (_camera == null) await initializeCamera();
-    var directory = await getApplicationDocumentsDirectory();
-    var filename = prettyPrintDigits(await numberOfExistingPictures());
-    var filePath = '${directory.path}/$filename.jpg';
-    try {
-      await _camera.takePicture(filePath);
-    } on CameraException catch (e) {
-      print('There was a problem taking the picture. $e');
-      return false;
-    }
-    return true;
   }
 
   @override
@@ -76,11 +45,7 @@ class _PhotographerState extends State<Photographer> {
       builder: (BuildContext context, AsyncSnapshot photosList) {
         return Column(
           children: <Widget>[
-            SimpleClock(TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Fascinate_Inline',
-              fontSize: 64,
-            )),
+            SimpleClock(artDecoStyle),
             Image.asset('assets/camera_top.png'),
             Filmstrip(photosList.hasData ? photosList.data : []),
             Stack(
@@ -96,24 +61,12 @@ class _PhotographerState extends State<Photographer> {
   }
 
   _cameraDirectionButton() {
-    _switchCameraDirection() {
-      setState(() => _cameraDirection =
-          (_cameraDirection == CameraLensDirection.back)
-              ? CameraLensDirection.front
-              : CameraLensDirection.back);
-    }
-
     return RaisedButton(
         color: Colors.white,
         child: Text(
-            _cameraDirection == CameraLensDirection.back
-                ? 'Take Selfie'
-                : 'Use Back Camera',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Fascinate_Inline',
-                fontSize: 32)),
-        onPressed: _switchCameraDirection);
+            _frontCamera ? 'Use Back Camera' : 'Take Selfie',
+            style: artDecoButtonStyle),
+        onPressed: _camera.switchDirection);
   }
 
   @override
@@ -136,26 +89,6 @@ class Filmstrip extends StatelessWidget {
           children: _photoPaths.map((String s) => FilmImage(s)).toList(),
         ),
       ),
-    );
-  }
-}
-
-class FilmImage extends StatelessWidget {
-  FilmImage(this.path);
-  final String path;
-
-  @override
-  Widget build(BuildContext context) {
-    var filmstrip = Image.asset('assets/filmstrip_edge.jpg', height: 20);
-    return Column(
-      children: [
-        filmstrip,
-        Expanded(child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Image.file(File(path)),
-        )),
-        filmstrip,
-      ],
     );
   }
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:camera/camera.dart';
 
 /// Returns a human-readable representation of a duration
 String prettyPrintDuration(Duration duration, [showSeparator = true]) {
@@ -21,4 +22,43 @@ Future<int> numberOfExistingPictures() async {
       .map<String>((FileSystemEntity file) => file.path)
       .toList();
   return photos.length;
+}
+
+class Camera {
+  CameraController _camera;
+  CameraLensDirection _cameraDirection = CameraLensDirection.front;
+
+  initializeCamera() async {
+    List<CameraDescription> cameraOptions = await availableCameras();
+    try {
+      var frontCamera = cameraOptions.firstWhere(
+          (description) => description.lensDirection == _cameraDirection,
+          orElse: () => cameraOptions.first);
+      _camera = CameraController(frontCamera, ResolutionPreset.low);
+      await _camera.initialize();
+    } on StateError catch (e) {
+      print('No camera found in the direction $_cameraDirection: $e');
+    }
+  }
+
+  takePicture() async {
+    if (_camera == null) await initializeCamera();
+    var directory = await getApplicationDocumentsDirectory();
+    var filename = prettyPrintDigits(await numberOfExistingPictures());
+    var filePath = '${directory.path}/$filename.jpg';
+    try {
+      await _camera.takePicture(filePath);
+    } on CameraException catch (e) {
+      print('There was a problem taking the picture. $e');
+      return false;
+    }
+    return true;
+  }
+
+  switchDirection() {
+    _cameraDirection =
+          (_cameraDirection == CameraLensDirection.back)
+              ? CameraLensDirection.front
+              : CameraLensDirection.back;
+  }
 }
